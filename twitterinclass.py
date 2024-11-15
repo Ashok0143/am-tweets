e773108ad2bad775b3b2c3453d1f077cb894e679
Ashok Medasani <88182384+Ashok0143@users.noreply.github.com>
2024-11-15 11:58:27 -0600

    Update twitterinclass.py

--- twitterinclass.py
+++ twitterinclass.py
@@ -29,7 +29,7 @@ import plotly.express as px
 
 # Path for data
 # https://raw.githubusercontent.com/Ashok0143/am-tweets/refs/heads/main/Tweets.csv
-tweets = pd.read_csv('https://raw.githubusercontent.com/Ashok0143/am-tweets/refs/heads/main/Tweets.csv')
+tweets = pd.read_csv('Tweets.csv')
 
 # Create the pie chart
 fig = px.pie(tweets, names='airline_sentiment', title='Count of Tweets by Sentiment')
@@ -45,6 +45,21 @@ airline_counts = tweets['airline'].value_counts()
 # Create the bar graph using Plotly
 fig = px.bar(airline_counts,
              x=airline_counts.index,
+             y=airline_counts.values,
+             labels={ 'x' : 'Airline', 'y': 'Number of Tweets'},
+             title='Count of Tweets by Airline')
+fig.show()
+
+# prompt: Tweets count by Airline
+
+# Assuming 'tweets' DataFrame is already loaded as in the previous code
+
+# Count of tweets for each airline
+airline_counts = tweets['airline'].value_counts()
+
+# Create the bar graph using Plotly
+fig = px.bar(airline_counts, 
+             x=airline_counts.index, 
              y=airline_counts.values,
              labels={ 'x' : 'Airline', 'y': 'Number of Tweets'},
              title='Count of Tweets by Airline')

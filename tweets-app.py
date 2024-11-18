c2ca06b61b870fba769af4f26a2f771031992423
Ashok Medasani <88182384+Ashok0143@users.noreply.github.com>
2024-11-18 01:37:44 -0600

    Update tweets-app.py

--- tweets-app.py
+++ tweets-app.py
@@ -12,12 +12,6 @@ tweets = pd.read_csv(tweets_url)
 # Set up Streamlit layout and title
 st.title("Twitter Sentiment Analysis by Airline")
 
-# Display the first few rows of data
-st.write("Sample of the Tweet Data:")
-st.write(tweets.head())
-
-# Display the dataset shape
-st.write("Dataset shape:", tweets.shape)
 
 # Pie Chart - Count of Tweets by Sentiment
 st.subheader("Count of Tweets by Sentiment")
@@ -28,14 +22,10 @@ st.plotly_chart(fig_pie)
 st.subheader("Count of Tweets by Airline")
 airline_counts = tweets['airline'].value_counts()
 
-# Custom color for each airline (adjust colors as needed)
-colors = px.colors.qualitative.Set3  # You can change this to any other color set available in Plotly
-
-fig_bar = px.bar(
-    x=airline_counts.index,
-    y=airline_counts.values,
-    labels={'x': 'Airline', 'y': 'Number of Tweets'},
-    title='Count of Tweets by Airline',
-    color_discrete_sequence=colors
-)
+fig = px.bar(airline_counts,
+             x=airline_counts.index,
+             y=airline_counts.values,
+             labels={ 'x' : 'Airline', 'y': 'Number of Tweets'},
+             title='Count of Tweets by Airline')
+fig.show()
 st.plotly_chart(fig_bar)
